878b57b2f7741de5f25fb68d7c5ff7a0e2cb0a40
hGoo-creator <hyungooshin@gmail.com>
2026-08-12 22:30:07 +0900

    Add files via upload

--- src/components/PlaceSearchInput.tsx
+++ src/components/PlaceSearchInput.tsx
@@ -81,7 +81,7 @@ export const PlaceSearchInput: React.FC<PlaceSearchInputProps> = ({
       setIsLoading(true);
       try {
         const response = await fetch(
-          `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&limit=5`
+          `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&limit=5&accept-language=ko`
         );
         const data = await response.json();
 
